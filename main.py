1d685b6a0d259f07b02b98ed44ad50164e0a708b
Leonardo Ribeiro <d86554300@gmail.com>
2026-02-05 20:41:10 -0300

    Added logic for history and logic to list the tasks.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.tools import tool
@@ -23,27 +24,50 @@ def add_task(task, description=None):
     todoist.add_task(content=task,
                      description=description)
 
-tools = [add_task]
+@tool
+def show_tasks():
+    """Show all tasks from todoist. Use this when the user wants to show all tasks."""
+    all_tasks = todoist.get_tasks()
+    tasks = []
+    for task in all_tasks:
+        tasks.append(task.content)
+    
+    return tasks
+
+tools = [add_task, show_tasks]
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    api_key=gemini_api_key, temperature=0.3
+    api_key=gemini_api_key,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    temperature=0.3
 )
 
 # Construct the prompt
-system_prompt = "You are a helpful assistant. You will help the user add tasks."
-user_input = "add task to buy some milk from a local shop with the description, the description was added by llm"
+system_prompt = """
+    You are a helpful assistant. 
+    You will help the user add tasks.
+    You will help the user show existing tasks.
+    """
 
 prompt_template = ChatPromptTemplate(
     [
         ("system", system_prompt),
-        ("user", user_input),
-        MessagesPlaceholder("agent_scratchpad")
+        ("user", "{input}"), # placeholder handled dynamically by lanchain,
+        MessagesPlaceholder("agent_scratchpad"),
+        MessagesPlaceholder("history")
     ]
 )
 
 agent = create_openai_tools_agent(llm, tools, prompt_template)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-response = agent_executor.invoke({"input": user_input})
 
-print(response["output"])
+history = []
+while True:
+    user_input = input("What the task name you want to build? ")
+    response = agent_executor.invoke({"input": user_input, "history": history})
+    print(response["output"])
+    history.append(HumanMessage(content=response["output"]))
+    history.append(AIMessage(content=response["output"]))
